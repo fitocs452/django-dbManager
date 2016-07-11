38856d2bc886d162b2bf75894f491b9389822675
adolfo-viaro <adolfo.morales@viaro.net>
2016-07-11 09:08:13 -0600

    Start module and configurate to use postgresql and also added the module url's to the project

---
+++ database_manager/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class DatabaseManagerConfig(AppConfig):
+    name = 'database_manager'
--- dbManager/settings.py
+++ dbManager/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'database_manager.apps.DatabaseManagerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,10 +77,15 @@ WSGI_APPLICATION = 'dbManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'db_manager',
+        'USER': 'postgres',
+        'PASSWORD': 'viaro',
+        'HOST': 'localhost'
     }
 }
+
 
 
 # Password validation
