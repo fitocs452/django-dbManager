2d5c96ed7f50e4effa63e657c8ab1b48b71f7216
adolfo-viaro <adolfo.morales@viaro.net>
2016-07-14 12:35:38 -0600

    Fix js code

--- database_manager/static/database_manager/js/clear.js
+++ database_manager/static/database_manager/js/clear.js
@@ -1,16 +1,8 @@
-function clearContents(element) {
-  element.value = '';
-}
-
-$(function() // execute once the DOM has loaded
-{
-
-  // wire up Add Item button click event
-  $("#clear-button").click(function(event)
-  {
-    event.preventDefault(); // cancel default behavior
-    $("#id_query").val('');
-    return false;
-    //... rest of add logic
-  });
+$(function() {
+    // wire up Add Item button click event
+    $("#clear-button").click(function(event) {
+        event.preventDefault(); // cancel default behavior
+        $("#id_query").val('');
+        return false;
+    });
 });
